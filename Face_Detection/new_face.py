e162871f11d0999daad1f462eccba370b7a1fd5e
Arjunan A <duraiarjun94@gmail.com>
2025-07-26 05:03:54 +0530

    face_detect

--- Face_Detection/new_face.py
+++ Face_Detection/new_face.py
@@ -1,7 +1,7 @@
 import cv2
-
+#algorithm for detecting face
 alg = "haarcascade_frontalface_default.xml"
-
+#Loading alg
 haar_cascade = cv2.CascadeClassifier(alg)
 
 cam = cv2.VideoCapture(0)
@@ -9,7 +9,7 @@ cam = cv2.VideoCapture(0)
 while True:
     _,img = cam.read()
     grayImg = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    face = haar_cascade.detectMultiScale(grayImg,1.3,4)
+    face = haar_cascade.detectMultiScale(grayImg,1.3,4) # detecting multiple faces
     for (x,y,w,h) in face:
         cv2.rectangle(img,(x,y),(x+w,y+h), (255,255,0),5)
     cv2.imshow("FaceDetection",img)
